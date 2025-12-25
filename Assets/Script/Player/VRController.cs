8dd62f4debd9bbbd45b5ab2897ccb8b7f1f8064b
STECH-GAME-05\gameprogram <a>
2025-12-25 17:04:35 +0900

    宝箱オブジェクトの作成中

--- Assets/Script/Player/VRController.cs
+++ Assets/Script/Player/VRController.cs
@@ -49,6 +49,12 @@ public class VRController : MonoBehaviour
     [SerializeField] HapticController haptic;
     [SerializeField] bool isRightHand = true;   //左右の判断
 
+    [Header("宝箱操作設定")]
+    [SerializeField] string chestTag = "Chest";
+    ChestLid currentChestLid;
+    float lastControllerY;
+
+
     // 入力アセット（自作の VRHookActions）
     VRHookActions HookMap;
 
@@ -157,13 +163,22 @@ public class VRController : MonoBehaviour
         ReadInputs();
         HandleCameraRotation();
 
-        if (!isGameRayEnabled)
-        {
-            SetCommonLineEnabled(false);
-            prevTriggerPressed = triggerPressed;
-            prevGripPressed = gripPressed;
-            return;
-        }
+        //CheckForChest();
+
+        //if(currentChestLid != null && triggerPressed)
+        //{
+        //    HandleChestInteraction();
+        //    return;
+        //}
+
+
+        //if (!isGameRayEnabled)
+        //{
+        //    SetCommonLineEnabled(false);
+        //    prevTriggerPressed = triggerPressed;
+        //    prevGripPressed = gripPressed;
+        //    return;
+        //}
 
         HandleTriggerPriority();
         UpdateStateMachine();
@@ -330,6 +345,43 @@ public class VRController : MonoBehaviour
             Debug.Log("[VRController] プレイヤーY軸をリセット (スティック押し込み)");
         }
     }
+
+
+    //宝箱がレイ上にあるかのチェック
+    //void CheckForChest()
+    //{
+    //    Ray ray = new Ray(rayOrigin.position,rayOrigin.forward);
+    //    if(Physics.Raycast(ray, out RaycastHit hit, maxWireLength))
+    //    {
+    //        if (hit.collider.CompareTag(chestTag))
+    //        {
+    //            currentChestLid = hit.collider.GetComponent<ChestLid>();
+    //            return;
+    //        }
+    //    }
+    //    if(!triggerPressed) currentChestLid = null;
+    //}
+
+
+    //void HandleChestInteraction()
+    //{
+    //    //前フレームからの高さ差分を計算
+    //    float currentY = rayOrigin.position.y;
+    //    if (prevTriggerPressed)
+    //    {
+    //        float deltaY = currentY - lastControllerY;
+
+    //        //蓋のスクリプトを回転
+    //        if(currentChestLid != null)
+    //        {
+    //            currentChestLid.UpdateRotation(deltaY);
+    //        }
+    //    }
+    //    //現在の高さを保存
+    //    lastControllerY = currentY;
+
+    //    if(commonLine != null) commonLine.enabled = false;
+    //}
 
     // -----------------------------
     // トリガー優先通過後の状態処理（グリップ系など）
